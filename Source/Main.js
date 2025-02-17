6dacf04b3077e3b0727708abe16fb2d83e8286b2
Daniella Fegi <114934334+Yuf0ria@users.noreply.github.com>
2025-02-18 01:46:52 +0800

    parallax complete;
    -Div & to be developed.

--- Source/Main.js
+++ Source/Main.js
@@ -1,15 +1,10 @@
 const parallax_el = document.querySelectorAll (".Parallax");
+const main = document.querySelector("main");
 
 let xValue = 0, yValue = 0;
 let rotateDegree = 0;
 
-window.addEventListener("mousemove", (e) => {
-
-    xValue = e.clientX - window.innerWidth / 2;
-    yValue = e.clientY - window.innerHeight / 2;
-
-    rotateDegree = (xValue / (window.innerWidth/2)) * 20;
-
+function update(cursorPosition) {
     parallax_el.forEach(el => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
@@ -18,8 +13,38 @@ window.addEventListener("mousemove", (e) => {
 
         let isInLeft = 
             parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1: -1;
-        let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+        let zValue = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
 
         el.style.transform = `perspective(3000px) translateZ(${zValue * speedz}px) rotateY(${rotateDegree * rotateSpeed}deg) translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
-    })
-})
+    });
+};
+
+update(0);
+
+window.addEventListener("mousemove", (e) => {
+
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    rotateDegree = (xValue / (window.innerWidth/2)) * 20;
+
+    update(e.clientX);
+});
+
+if (window.innerWidth >= 725){
+    main.style.maxHeight =`${window.innerwidth * 0.6}px`;
+} else {
+    main.style.maxHeight =`${window.innerwidth * 1.6}px`;
+}
+
+/*GSAP animation*/
+
+let timeline = gsap.timeline();
+
+Array.from(parallax_el).filter(el => !el.classList.contains(".text")).forEach(el => {
+    timeline.from(el,{
+        top:`${el.offsetHeight / 2 + el.dataset.distnce}px`,
+        duration: 4.5,
+    });
+    "1"
+});
